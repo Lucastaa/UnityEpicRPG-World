f2179a66c4381887d0836be3c05744e564f6e959
Lucastaa <76867086+Lucastaa@users.noreply.github.com>
2025-10-19 16:38:23 +0700

    Add audio, fonts, scenes, and UI assets
    Added various audio files, font assets, TextMesh Pro examples, and UI prefabs. Renamed SampleScene to GamePlay and MainMenu, and updated Player.cs. These changes set up core assets and scripts for game functionality and UI.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -19,9 +19,6 @@ public class Player : MonoBehaviour
     // FixedUpdate duoc goi theo khoang thoi gian co dinh, tuy vao toc do khung hinh
     void FixedUpdate()
     {
-        //bool forwardPressed = inputVec.x > 0;
-        //bool ;
-        //if () { }
         Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
     }
